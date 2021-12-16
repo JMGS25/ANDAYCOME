8656b195e134dd0ae35964f7f05d4f8c3c6eae58
martap1862 <91124113+martap1862@users.noreply.github.com>
2021-12-16 09:54:28 +0100

    Add files via upload

---
+++ WebAndayComeGit/Models/WallViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAndayCome.Models
+{
+    public class WallViewModel
+    {
+        [ScaffoldColumn(false)] //para no mostrar la Clave primaria
+        public int Id { get; set; }
+
+        [Display(Prompt = "Muro", Description = "Comentarios del muro", Name = "Muro")]
+        [Required(ErrorMessage = "No se encuentra el muro")]
+        public CommentViewModel CommentViewModel { get; set; }
+}
+}
