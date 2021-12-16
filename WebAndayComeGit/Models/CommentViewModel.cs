99e03684e150aa2359013dfe26c3190d16ec73ec
martap1862 <91124113+martap1862@users.noreply.github.com>
2021-12-16 09:53:52 +0100

    Add files via upload

---
+++ WebAndayComeGit/Models/CommentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAndayCome.Models
+{
+    public class CommentViewModel
+    {
+        [ScaffoldColumn(false)] //para no mostrar la Clave primaria
+        public int Id { get; set; }
+
+        [Display(Prompt = "Autor del comentario", Description = "Contenido del comentario", Name = "Client")]
+        [Required(ErrorMessage = "Debes rellenar este campo")]
+        [StringLength(maximumLength: 150, ErrorMessage = "El nombre no puede tener mas de 200 caracteres")]
+        public string Client { get; set; }
+
+        [Display(Prompt = "Fecha del comentario", Description = "Contenido del comentario", Name = "Date")]
+        [Required(ErrorMessage = "Debes rellenar este campo")]
+       
+        public Nullable<DateTime> Date { get; set; }
+
+        [Display(Prompt = "Contenido del comentario", Description = "Contenido del comentario", Name = "Content")]
+        [Required(ErrorMessage = "Debes rellenar este campo")]
+        [StringLength(maximumLength: 500, ErrorMessage = "El nombre no puede tener mas de 200 caracteres")]
+        public string Content { get; set; }
+
+        [Display(Prompt = "Foto del usuario", Description = "Foto del usuario", Name = "Photo")]
+        [Required(ErrorMessage = "Debes indicar la direccion de la foto")]
+        public string Photo { get; set; }
+    }
+}
