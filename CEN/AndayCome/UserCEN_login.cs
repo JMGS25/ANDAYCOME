83614fa70735ea41991b21783f9685b57a0e45c9
SecretBritt <93913593+SecretBritt@users.noreply.github.com>
2021-12-16 10:09:09 +0100

    Add files via upload

--- CEN/AndayCome/UserCEN_login.cs
+++ CEN/AndayCome/UserCEN_login.cs
@@ -19,5 +19,21 @@ namespace AndayComeGenNHibernate.CEN.AndayCome
 {
 public partial class UserCEN
 {
+public string Login (string p_email, string p_pass)
+{
+        /*PROTECTED REGION ID(AndayComeGenNHibernate.CEN.AndayCome_User_login) ENABLED START*/
+        string result = null;
+
+        IList<UserEN> listaEN = DameUsuarioPorEmail (p_email);
+
+        if (listaEN.Count > 0) {
+                UserEN userEN = listaEN [0];
+                if (userEN.Pass.Equals (Utils.Util.GetEncondeMD5 (p_pass))) {
+                        result = this.GetToken (userEN.Name);
+                }
+        }
+        return result;
+        /*PROTECTED REGION END*/
+}
 }
 }
