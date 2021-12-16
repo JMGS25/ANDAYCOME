0030171ec6c7325d4ef682d6736007e8dd6ce05e
SecretBritt <93913593+SecretBritt@users.noreply.github.com>
2021-12-16 10:08:07 +0100

    Add files via upload

---
+++ WebAndayComeGit/Models/UserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAndayCome.Models
+{
+    public class UserViewModel : RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Teléfono")]
+
+        public int telefono { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+
+        public string nombre { get; set; }
+
+
+        [Required]
+        [Display(Name = "Ciudad")]
+
+        public int ciudad { get; set; }
+
+
+
+        [Required]
+        [Display(Name = "Foto")]
+
+        public string foto { get; set; }
+    }
+}
