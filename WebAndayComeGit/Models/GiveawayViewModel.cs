d7dba4f7df18e1a758b699f82d18cd949e87f3c6
Lybraris <79850627+Lybraris@users.noreply.github.com>
2021-12-16 20:12:20 +0100

    Add files via upload

---
+++ WebAndayComeGit/Models/GiveawayViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAndayCome.Models
+{
+    public class GiveawayViewModel
+    {
+
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+
+        [Display(Prompt = "Premio del sorteo", Description = "Premio del sorteo", Name = "Premio ")]
+        public string Premio { get; set; }
+
+        [Display(Prompt = "Ganador de sorteo", Description = "Ganador de sorteo", Name = "Ganador ")]
+        public string Winner { get; set; }
+
+        [Display(Prompt = "Precio del sorteo", Description = "Precio del sorteo", Name = "Precio ")]
+        [Required(ErrorMessage = "Debe haber un precio")]
+        public int Precio { get; set; }
+
+        [Display(Prompt = "Usuario premium", Description = "Usuario premium", Name = "Premium ")]
+        public bool Premium { get; set; }
+    }
+}
