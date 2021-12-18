b14a078783b44573f7095b1e1287d1f2b9cdc6e4
Juan Manuel <72016988+JMGS25@users.noreply.github.com>
2021-12-18 01:03:24 +0100

    Add files via upload

---
+++ RestaurantController.cs
@@ -0,0 +1,176 @@
+﻿using AndayComeGenNHibernate.CAD.AndayCome;
+using AndayComeGenNHibernate.CEN.AndayCome;
+using AndayComeGenNHibernate.CP.AndayCome;
+using AndayComeGenNHibernate.EN.AndayCome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAndayCome.Models;
+using WebAndayCome.Assemblers;
+using System.IO;
+
+namespace WebAndayCome.Controllers
+{
+    public class RestaurantController : BasicController
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            SessionInitialize(); //si navegamos entre distintas clases es necesario
+            RestaurantCAD restaurantCAD = new RestaurantCAD(session);
+            RestaurantCEN restaurantCEN = new RestaurantCEN(restaurantCAD);
+
+            IList<RestaurantEN> listEN = restaurantCEN.ReadAll(0, -1);
+            IEnumerable<RestaurantViewModel> listViewModel = new RestaurantAssembler().ConvertListENToModel(listEN).ToList();
+            SessionClose();
+            return View(listViewModel);
+        }
+
+        // GET: Restaurant/Details/5
+        public ActionResult Details(int id)
+        {
+            RestaurantViewModel res = null;
+            SessionInitialize();
+            RestaurantEN resEN = new RestaurantCAD(session).ReadOIDDefault(id);
+            res = new RestaurantAssembler().ConvertENToModelUI(resEN);
+            SessionClose();
+            return View(res);
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            try
+            {
+                IList<Restaurant_OwnerEN> listaPropietarios = new Restaurant_OwnerCEN().ReadAll(0, -1);
+                IList<SelectListItem> propietariosItems = new List<SelectListItem>();
+
+                foreach (Restaurant_OwnerEN rest in listaPropietarios)
+                {
+                    propietariosItems.Add(new SelectListItem { Text = rest.Email, Value = rest.Email });
+                }
+
+                IList<CountryEN> listaPaises = new CountryCEN().ReadAll(0, -1);
+                IList<SelectListItem> countryitems = new List<SelectListItem>();
+
+                foreach (CountryEN country in listaPaises)
+                {
+                    countryitems.Add(new SelectListItem { Text = country.City.ToString(), Value = country.Id.ToString() });
+                }
+
+                ViewData["idPais"] = countryitems;
+                ViewData["idPropietario"] = propietariosItems;
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        public ActionResult Create(RestaurantViewModel res, HttpPostedFileBase file)
+        {
+            string fileName = "", path = "";
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                path = Path.Combine(Server.MapPath("~/Images/Uploads"), fileName);
+                //string pathDef = path.Replace(@"\\", @"\");
+                file.SaveAs(path);
+            }
+
+            try
+            {
+                fileName = "/Images/Uploads/" + fileName;
+                RestaurantCEN cen = new RestaurantCEN();
+                cen.New_("ib50@gmail.com", 2, res.Menu, res.Photo, fileName, res.Street, res.Capacity); //valores de prueba para restaurantOwner y country
+
+                 return RedirectToAction("Index");
+                //return View("Detail", new { id = res.Id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurant/Edit/5
+        public ActionResult Edit(int id)
+        {
+            RestaurantViewModel res = null;
+            SessionInitialize();
+            RestaurantEN artEN = new RestaurantCAD(session).ReadOIDDefault(id);
+            res = new RestaurantAssembler().ConvertENToModelUI(artEN);
+            SessionClose();
+            return View(res);
+        }
+
+        // POST: Restaurant/Edit/5
+        [HttpPost]
+        public ActionResult Edit(RestaurantViewModel res)
+        {
+            try
+            {
+                RestaurantCEN cen = new RestaurantCEN();
+                cen.Modify(res.Id, res.Menu, res.Photo, res.Name, res.Street, res.Capacity);
+
+                return RedirectToAction("Details", new { id = res.Id});
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurant/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                int idCategoria = -1;
+                SessionInitialize();
+                RestaurantCAD resCAD = new RestaurantCAD(session);
+                RestaurantCEN cen = new RestaurantCEN(resCAD);
+                RestaurantEN resEN = cen.ReadOID(id);
+                RestaurantViewModel res = new RestaurantAssembler().ConvertENToModelUI(resEN);
+                idCategoria = res.Id;
+                SessionClose();
+
+                new RestaurantCEN().Destroy(id);
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        // POST: Restaurant/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RestaurantViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAndayCome.Models
+{
+    public class RestaurantViewModel
+    {
+        [ScaffoldColumn(false)] //para no mostrar la Clave primaria
+        public int Id { get; set; }
+
+
+        [Display(Prompt = "Menu del restaurante", Description = "Menu del restaurante", Name = "Menú")]
+        [Required(ErrorMessage = "Debes indicar la direccion de la foto")]
+        public string Menu { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Display(Prompt = "Ciudad en el que se encuentra el restaurante", Description = "Ciudad en el que se encuentra el restaurante", Name = "Ciudad ")]
+        public int IdPais { get; set; }
+
+        [Display(Prompt = "Posibles propietarios", Description = "Posibles propietarios", Name = "Propietario ")]
+        public string IdPropietario { get; set; }
+
+
+        [Display(Prompt = "Ciudad del restaurante", Description = "Ciudad del restaurante", Name = "Ciudad")]
+        public string City { get; set; }
+
+
+        [Display(Prompt = "Menu del restaurante", Description = "Menu del restaurante", Name = "Photo")]
+        [Required(ErrorMessage = "Debes indicar la direccion de la foto")]
+        public string Photo { get; set; }
+
+        [Display(Prompt = "Nombre del restaurante", Description = "Nombre del restaurante", Name = "Nombre")]
+        [Required(ErrorMessage = "Debes indicar un nombre para el restaurante")]
+        [StringLength(maximumLength: 200, ErrorMessage = "El nombre no puede tener mas de 200 caracteres")]
+        public string Name { get; set; }
+
+
+        [Display(Prompt = "Calle del restaurante", Description = "Calle del restaurante", Name = "Calle")]
+        [Required(ErrorMessage = "Debes indicar una calle para el restaurante")]
+        [StringLength(maximumLength: 200, ErrorMessage = "La calle no puede tener mas de 200 caracteres")]
+        public string Street { get; set; }
+
+
+
+        [ScaffoldColumn(false)]
+        [Display(Prompt = "Tags del restaurante", Description = "Tags del restaurante", Name = "Tags")]
+        public string Tags { get; set; }
+
+
+        [Display(Prompt = "Capacidad del restaurante", Description = "Capacidad del restaurante", Name = "Capacidad")]
+        [Required(ErrorMessage = "Debes indicar una capacidad para el restaurante")]
+
+        [Range(minimum: 0, maximum: 1000, ErrorMessage = "La capacidad debe ser entre 0 y 1000")]
+        public int Capacity { get; set; }
+
+
+
+    }
+}
